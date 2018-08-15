385f7ad76e0f3cf8909385bac8b01cf94b8cbd49
Thomas Altmann <thomasaltmann@Thomass-MacBook-Pro.local>
2018-08-15 11:39:47 -0400

    TEST 2

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ var http, director, bot, router, server, port;
 http        = require('http');
 director    = require('director');
 bot         = require('./bot.js');
-var express = require('express');
-var app   = express();
-
 
 router = new director.http.Router({
   '/' : {
@@ -35,20 +32,4 @@ function ping() {
   this.res.end();
   
 }
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
 
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM nwhs_groupme');
-    res.render('pages/db', result);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
