0dd23fb3e3443048f41cffdbd624de5854fc29de
Thomas Altmann <thomasaltmann@Thomass-MacBook-Pro.local>
2018-08-19 12:53:06 -0400

    TEST 90

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 //Groupme bot POST service
 app.post('/', function(req, res){
-    req.push(bot.respond.toString());
+    res.send(bot.respond);
 });
 
 app.get('/', function(req, res){
